4bf0ee712c2bdb4090ecb2f4d03aeefadb9b92e4
root <root@node1.cluster.local>
2021-05-31 05:59:37 +0000

    redis cluster

---
+++ redis/start-cluster.sh
@@ -0,0 +1,6 @@
+sudo redis-server redis-0/redis.conf && \
+sudo redis-server redis-1/redis.conf && \
+sudo redis-server redis-2/redis.conf && \
+sudo redis-server redis-3/redis.conf && \
+sudo redis-server redis-4/redis.conf && \
+sudo redis-server redis-5/redis.conf
---
+++ redis/stop-cluster.sh
@@ -0,0 +1,6 @@
+sudo kill -9 $(lsof -t -i:8000) && \
+sudo kill -9 $(lsof -t -i:8001) && \
+sudo kill -9 $(lsof -t -i:8002) && \
+sudo kill -9 $(lsof -t -i:8003) && \
+sudo kill -9 $(lsof -t -i:8004) && \
+sudo kill -9 $(lsof -t -i:8005) 
